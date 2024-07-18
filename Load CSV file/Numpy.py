a884ad12f0ad28bd6bf0682959332053e6648ee7
ISE <122774225+S-tni@users.noreply.github.com>
2024-07-18 23:55:53 +0700

    Update Numpy.py

--- Load CSV file/Numpy.py
+++ Load CSV file/Numpy.py
@@ -7,7 +7,7 @@ raw_data = request.urlopen(filename)'''
 
 # code to load CSV (comma-separated values) file 
 
-filename = 'D:\Important_Data\Code\pima-indians-diabetes.csv' #file directort
+filename = 'pima-indians-diabetes.csv' #file directort
 raw_data = open(filename, 'rb') #open file, set read as binary mode
 data = np.loadtxt(raw_data, delimiter=",") # delimeter tell NumPy to split the values in each line based on commas
 print(data.shape) 
