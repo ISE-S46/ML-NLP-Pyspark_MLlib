e7522947b8828afcda3669d0ec36783dab5186e5
S-tni <ise.sns46@gmail.com>
2024-08-06 23:14:14 +0700

    edit rescale data.py

--- 4 Prepare Data/Rescale Data.py
+++ 4 Prepare Data/Rescale Data.py
@@ -16,3 +16,12 @@ rescaledX = scaler.fit_transform(X) # calculates the scaling parameters based on
 # summarize transformed data
 set_printoptions(precision=3) # when the transformed data is printed, it will be rounded to 3 decimal places
 print(rescaledX[0:5,:]) # print first 5 row of transformed data
+
+# Output
+'''
+[[0.353 0.744 0.59  0.354 0.    0.501 0.234 0.483] 
+ [0.059 0.427 0.541 0.293 0.    0.396 0.117 0.167] 
+ [0.471 0.92  0.525 0.    0.    0.347 0.254 0.183] 
+ [0.059 0.447 0.541 0.232 0.111 0.419 0.038 0.   ] 
+ [0.    0.688 0.328 0.354 0.199 0.642 0.944 0.2  ]]
+ '''
