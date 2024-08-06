ae6e2be39594ac4971c611f9e2c4af8a553f4fca
S-tni <ise.sns46@gmail.com>
2024-08-06 23:43:31 +0700

    update multivariate folder

---
+++ 4 Understand Data With Visualization/Multivariate Plots/2 Scatter Plot Matrix.py
@@ -0,0 +1,10 @@
+from pandas import read_csv
+from matplotlib import pyplot
+from pandas.plotting import scatter_matrix
+
+filename = 'D:\Important_Data\Code\diabetes.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names, skiprows=1) # setting skiprows=1, telling Pandas to start reading the data from the second row onwards
+
+scatter_matrix(data)
+pyplot.show()
