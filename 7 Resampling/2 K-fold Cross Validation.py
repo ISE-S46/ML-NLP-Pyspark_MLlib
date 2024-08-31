83e41aba926b92095d030108e4266be64ce217ae
S-tni <ise.sns46@gmail.com>
2024-08-31 21:31:27 +0700

    Update resampling

---
+++ 7 Resampling/2 K-fold Cross Validation.py
@@ -0,0 +1,23 @@
+# Evaluate using Cross Validation
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+# load data
+filename = 'diabetes.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(filename, names=names, skiprows=1) 
+array = dataframe.values
+X = array[:,0:8] 
+Y = array[:,8] 
+num_folds = 10
+seed = 7
+kfold = KFold(n_splits=num_folds, shuffle=True, random_state=seed)
+model = LogisticRegression(max_iter=1000)
+results = cross_val_score(model, X, Y, cv=kfold)
+print("Accuracy: %.3f%% (%.3f%%)" % (results.mean()*100.0, results.std()*100.0))
+
+# Output
+'''
+Accuracy: 77.216% (4.968%)
+'''
